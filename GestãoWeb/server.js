dbfcc053c4e19ea7255670abc28262e1194e1401
🏢 PyDen Tech <167042827+PyDenTech@users.noreply.github.com>
2024-07-14 19:52:35 +0000

    Ataulização em rota escolar

--- GestãoWeb/server.js
+++ GestãoWeb/server.js
@@ -1071,12 +1071,12 @@ app.delete('/api/motoristasescolares/:id', async (req, res) => {
 });
 
 app.post('/api/registerMotoristasEscolares', async (req, res) => {
-    const { nome_completo, cpf, cnh, tipo_veiculo, placa, empresa, email, senha, longitude, latitude } = req.body;
+    const { nome_completo, cpf, cnh, tipo_veiculo, placa, empresa, email, senha } = req.body;
   
     try {
       const result = await pool.query(
-        'INSERT INTO motoristasescolares (nome_completo, cpf, cnh, tipo_veiculo, placa, empresa, email, senha, longitude, latitude) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
-        [nome_completo, cpf, cnh, tipo_veiculo, placa, empresa, email, senha, longitude, latitude]
+        'INSERT INTO motoristasescolares (nome_completo, cpf, cnh, tipo_veiculo, placa, empresa, email, senha) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+        [nome_completo, cpf, cnh, tipo_veiculo, placa, empresa, email, senha]
       );
       res.status(201).json(result.rows[0]);
     } catch (err) {
