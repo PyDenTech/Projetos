3214ab0e46d06d6469848f83c8abc7228080a3df
PyDen Technologies <pydentech@gmailcom>
2024-07-22 17:28:18 -0300

    Atualização de recuperação de senhas

--- GestãoWeb/server.js
+++ GestãoWeb/server.js
@@ -136,9 +136,9 @@ app.post('/api/upload-foto-perfil', ensureLoggedIn, upload.single('foto_perfil')
 
 // Configuração do Nodemailer para enviar e-mails
 const transporter = nodemailer.createTransport({
-    host: 'smtp.hostinger.com', // ou o host SMTP fornecido pela Hostinger
-    port: 465, // ou 465 se você estiver usando SSL
-    secure: true, // true para 465, false para outras portas
+    host: 'smtp.hostinger.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
