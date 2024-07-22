a01e563c4b6d91e7b580335f792f3c9220b1e673
PyDen Technologies <pydentech@gmailcom>
2024-07-22 16:47:39 -0300

    Teste de fornecedor

--- GestãoWeb/server.js
+++ GestãoWeb/server.js
@@ -1224,6 +1224,47 @@ app.get('/api/fornecedores/:id', async (req, res) => {
     }
 });
 
+// Endpoint para editar um fornecedor específico
+app.put('/api/fornecedores/:id', async (req, res) => {
+    const id = req.params.id;
+    const { nomeFornecedor, tipoContrato, cnpjFornecedor, contatoFornecedor, latitude, longitude, endereco } = req.body;
+
+    try {
+        const result = await pool.query(
+            'UPDATE fornecedores SET nome_fornecedor = $1, tipo_contrato = $2, cnpj_fornecedor = $3, contato_fornecedor = $4, latitude = $5, longitude = $6, endereco = $7 WHERE id = $8 RETURNING *',
+            [nomeFornecedor, tipoContrato, cnpjFornecedor, contatoFornecedor, latitude, longitude, endereco, id]
+        );
+
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]);
+        } else {
+            res.status(404).json({ error: 'Fornecedor não encontrado' });
+        }
+    } catch (error) {
+        console.error('Erro ao editar fornecedor:', error);
+        res.status(500).json({ error: 'Erro ao editar fornecedor' });
+    }
+});
+
+// Endpoint para excluir um fornecedor específico
+app.delete('/api/fornecedores/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const result = await pool.query('DELETE FROM fornecedores WHERE id = $1 RETURNING *', [id]);
+
+        if (result.rows.length > 0) {
+            res.json({ message: 'Fornecedor excluído com sucesso' });
+        } else {
+            res.status(404).json({ error: 'Fornecedor não encontrado' });
+        }
+    } catch (error) {
+        console.error('Erro ao excluir fornecedor:', error);
+        res.status(500).json({ error: 'Erro ao excluir fornecedor' });
+    }
+});
+
+
 // Endpoint para obter todas as escolas
 app.get('/api/escolas', async (req, res) => {
     try {
