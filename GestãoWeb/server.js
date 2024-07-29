62004ce68e76d35f8415dc75f19524cd488bc4f1
PyDen Technologies <pydentech@gmailcom>
2024-07-29 17:59:00 -0300

    Atualização de servidor para cancelar demanda

--- GestãoWeb/server.js
+++ GestãoWeb/server.js
@@ -1928,6 +1928,24 @@ app.post('/api/atualizarLocalizacao', async (req, res) => {
     }
 });
 
+app.get('/api/motoristas/:id/localizacao', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await pool.query('SELECT latitude, longitude FROM motoristas_administrativos WHERE id = $1', [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Motorista não encontrado' });
+        }
+
+        const { latitude, longitude } = result.rows[0];
+        res.json({ latitude, longitude });
+    } catch (error) {
+        console.error('Erro ao obter a localização do motorista:', error);
+        res.status(500).json({ error: 'Erro ao obter a localização do motorista' });
+    }
+});
+
 app.post('/api/avisos', async (req, res) => {
     const { titulo, mensagem, destinatario } = req.body;
 
