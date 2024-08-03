12f9a7a2805322d3f720f6bce80144356301d4f6
Danilo de Morais <pydentech@gmail.com>
2024-08-03 20:36:14 -0300

    Atualização de administrador

--- GestãoWeb/server.js
+++ GestãoWeb/server.js
@@ -169,6 +169,9 @@ app.get('/redefinir-senha/:token', (req, res) => {
 app.get('/dashboard-adm', ensureLoggedIn, (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'pages', 'dashboard-adm.html'));
 });
+app.get('/cadastrar-bairros-form', ensureLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'pages', 'cadastrar-bairros-form.html'));
+});
 
 app.get('/solicitar-redefinir-senha', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'solicitar-redefinir-senha.html'));
@@ -254,7 +257,6 @@ const pages = [
     'gerenciar-fornecedores-view',
     'cadastrar-motorista-form',
     'cadastrar-motorista-carro-form',
-    'cadastrar-bairros-form'
 ];
 
 const pageRoles = {
