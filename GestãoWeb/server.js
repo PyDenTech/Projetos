b241fa79e2a87b792b05ff44a90f42bb50db70b0
Danilo de Morais <pydentech@gmail.com>
2024-08-10 13:30:27 -0300

    Teste de bot twilio

--- GestãoWeb/server.js
+++ GestãoWeb/server.js
@@ -2286,27 +2286,9 @@ app.post('/api/cadastrar-abastecimento-administrativos', async (req, res) => {
 app.get('/api/motoristas_escolares', async (req, res) => {
     try {
         const result = await pool.query(`
-        SELECT 
-            m.id, 
-            m.nome_completo, 
-            m.cpf, 
-            m.cnh, 
-            m.empresa, 
-            m.veiculo, 
-            m.placa, 
-            m.certificado_transporte, 
-            m.certificado_escolar, 
-            m.documento_cnh, 
-            array_agg(r.id) AS rota_ids, 
-            array_agg(r.nome_rota) AS rota_nomes
-        FROM 
-            motoristas_escolares m
-        LEFT JOIN 
-            motorista_rotas mr ON m.id = mr.motorista_id
-        LEFT JOIN 
-            rotas r ON mr.rota_id = r.id
-        GROUP BY 
-            m.id
+        SELECT m.id, m.nome_completo, m.cpf, m.cnh, m.empresa, m.veiculo, m.placa, m.certificado_transporte, m.certificado_escolar, m.documento_cnh, r.id AS rota_id, r.nome_rota AS rota_nome
+        FROM motoristas_escolares m
+        LEFT JOIN rotas r ON m.rota_id = r.id
       `);
         res.status(200).json(result.rows);
     } catch (error) {
