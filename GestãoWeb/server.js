93bf7323dbedb01b30ae9fdaa1f32b30447f631a
Danilo de Morais <pydentech@gmail.com>
2024-08-15 19:01:28 -0300

    Teste de bot twilio

--- GestãoWeb/server.js
+++ GestãoWeb/server.js
@@ -2898,34 +2898,25 @@ app.get('/api/verificar-direito-transporte', async (req, res) => {
 
 // BOT DO SETOR DE TRANSPORTE
 
-app.post('/consulta-aluno', async (req, res) => {
-    const idMatricula = req.body.id_matricula;
+app.post('/api/verificar-id', async (req, res) => {
+    const { id_aluno } = req.body;
 
-    console.log('ID de Matrícula Recebido:', idMatricula); // Adicione este log
+    if (!id_aluno) {
+        return res.status(400).json({ status: 'fail', message: 'ID do aluno não fornecido' });
+    }
 
     try {
-        const query = 'SELECT nome, dt_nascimento, endereco FROM alunos WHERE id_matricula = $1';
-        const values = [idMatricula];
-        const result = await pool.query(query, values);
+        const query = 'SELECT id FROM public.alunos WHERE id = $1';
+        const result = await pool.query(query, [id_aluno]);
 
         if (result.rows.length > 0) {
-            const aluno = result.rows[0];
-            const formattedDate = format(new Date(aluno.dt_nascimento), 'dd-MM-yyyy');
-
-            res.json({
-                status: 'success',
-                aluno: {
-                    nome: aluno.nome,
-                    dt_nascimento: formattedDate,
-                    endereco: aluno.endereco,
-                }
-            });
+            res.status(200).json({ status: 'success', message: 'ID encontrado' });
         } else {
-            res.json({ status: 'not_found', message: 'Aluno não encontrado.' });
+            res.status(404).json({ status: 'fail', message: 'ID não encontrado' });
         }
     } catch (error) {
-        console.error('Erro ao consultar aluno:', error);
-        res.status(500).json({ status: 'error', message: 'Erro interno do servidor.' });
+        console.error('Erro ao verificar ID:', error);
+        res.status(500).json({ status: 'error', message: 'Erro no servidor' });
     }
 });
 
